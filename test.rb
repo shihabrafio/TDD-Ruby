96d4c0094257faa55c8c26147663bc7108ed9c3b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-03 15:41:23 +0600

    Fix Linter Error

--- test.rb
+++ test.rb
@@ -1,16 +1,15 @@
 require './solver'
-solver =Solver.new
+solver = Solver.new
 
 describe 'factorial' do
-    it 'Factorial of a number' do
-      expect(solver.factorial(4)).to eq(24)
-    end
-    it 'Factorial of zero' do
-      expect(solver.factorial(0)).to eq(1)
-    end
-  
-    it 'Factorial of a negative number' do
-      expect { solver.factorial(-7) }.to raise_error(ArgumentError)
-    end
+  it 'Factorial of a number' do
+    expect(solver.factorial(4)).to eq(24)
+  end
+  it 'Factorial of zero' do
+    expect(solver.factorial(0)).to eq(1)
+  end
+
+  it 'Factorial of a negative number' do
+    expect { solver.factorial(-7) }.to raise_error(ArgumentError)
   end
 end
